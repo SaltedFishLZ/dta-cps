cf8235ecede2d2497450e6e1ce3aa100f9fec474
SaltedFishLZ <zhliang@berkeley.edu>
2020-12-13 18:37:09 +0800

    FlexPRET docker setup scripts/Dockerfiles added, need to be tested

---
+++ docker/flexpret/original/setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+apt-get update && \
+apt-get install -yqq vim wget unzip curl gawk git && \
+apt-get update && \
+apt-get install -yqq autoconf automake autotools-dev \
+    libmpc-dev libmpfr-dev libgmp-dev libusb-1.0-0-dev \
+    build-essential bison flex texinfo gperf \
+    libtool patchutils bc zlib1g-dev \
+    device-tree-compiler \
+    pkg-config \
+    cmake cmake-doc \
+    python python3 \
+    bsdmainutils && \
+apt update && \
+apt install -y default-jdk
+
+# download and setup flexpret
+
+REPO_OWNER=saltedfishlz
+REPO_NAME=flexpret
+
+git clone https://github.com/${REPO_OWNER}/${REPO_NAME}.git && \
+cd flexpret && \
+git checkout RTAS14
